b551d2044cfa62df7e4054b11fbb208709bfca25
rinfiyks <tom.gould.92@gmail.com>
2020-12-06 15:19:13 +0000

    Day 6

---
+++ python/day6.py
@@ -0,0 +1,44 @@
+from common import read_file, timer
+
+
+@timer
+def part_1():
+    input_data = read_file('day6')
+    input_data.append('')
+
+    s = ''
+    count = 0
+    for l in input_data:
+        if l == '':
+            count += len(set(s))
+            s = ''
+        else:
+            s += l
+    print(count)
+
+
+@timer
+def part_2():
+    input_data = read_file('day6')
+    input_data.append('')
+
+    s = set()
+    count = 0
+    flag = True
+    for l in input_data:
+        if l == '':
+            count += len(s)
+            s = set()
+            flag = True
+        else:
+            if flag:
+                s = set(l)
+                flag = False
+            else:
+                s = s.intersection(set(l))
+
+    print(count)
+
+
+part_1()
+part_2()
